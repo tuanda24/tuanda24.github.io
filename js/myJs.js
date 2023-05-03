054cda74e2e60ec58c7a3560d59017a783dceb91
Admin <tuanda@sunshinegroup.vn>
2023-05-03 21:47:07 +0700

    update

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/tuan.dg.24/";
+            window.location = "heart.html";
           },
         });
       }
