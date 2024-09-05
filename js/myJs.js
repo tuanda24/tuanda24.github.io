ae4a95fa826411345c53af54374014855afc2595
tuanda24 <90612130+tuanda24@users.noreply.github.com>
2024-09-05 11:36:04 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://tuanda24.github.io/heart.html";
+            window.location = "https://www.facebook.com/tuan.dg.24";
           },
         });
       }
