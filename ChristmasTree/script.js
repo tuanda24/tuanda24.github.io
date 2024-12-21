41a27d9876235567e9e7c769ab634fd9b4ca1020
tuanda24 <dat.2000.dz@gmail.com>
2024-12-21 23:42:52 +0700

    save

--- ChristmasTree/script.js
+++ ChristmasTree/script.js
@@ -5,3 +5,72 @@ y:-100,transformOrigin:"50% 50%"})})();(function(){k=gsap.timeline({onUpdate:t})
 ".treePotMask"],{drawSVG:"0% 0%",stroke:"#FFF",stagger:{each:6},duration:gsap.utils.wrap([6,1,2]),ease:"linear"}).from(".treeStar",{duration:3,scaleY:0,scaleX:.15,transformOrigin:"50% 50%",ease:"elastic(1,0.5)"},"-=4").to(".sparkle",{duration:3,opacity:0,ease:"rough({strength: 2, points: 100, template: linear, taper: both, randomize: true, clamp: false})"},"-=0").to(".treeStarOutline",{duration:1,opacity:1,ease:"rough({strength: 2, points: 16, template: linear, taper: none, randomize: true, clamp: false})"},
 "+=1");c.add(k,0);gsap.globalTimeline.timeScale(1.5); k.vars.onComplete = function() { gsap.to('#endMessage', { opacity: 1 }) } })();
 
+jQuery(function () {
+	var d = function () {};
+	jQuery(document)
+	  .delegate(".b-ball_bounce", "mouseenter", function () {
+		b(this);
+		m(this);
+	  })
+	  .delegate(".b-ball_bounce .b-ball__right", "mouseenter", function (i) {
+		i.stopPropagation();
+		b(this);
+		m(this);
+	  });
+  
+	var g = 36;
+  
+	function b(n) {
+	  if (n.className.indexOf("b-ball__right") > -1) {
+		n = n.parentNode;
+	  }
+	  var i = /b-ball_n(\d+)/.exec(n.className);
+	  var j = /b-head-decor__inner_n(\d+)/.exec(n.parentNode.className);
+	  if (i && j) {
+		i = parseInt(i[1], 10) - 1;
+		j = parseInt(j[1], 10) - 1;
+		d((i + j * 9) % g);
+	  }
+	}
+	function m(j) {
+	  var i = jQuery(j);
+	  if (j.className.indexOf(" bounce") > -1) {
+		return;
+	  }
+	  i.addClass("bounce");
+  
+	  function n() {
+		i.removeClass("bounce").addClass("bounce1");
+  
+		function o() {
+		  i.removeClass("bounce1").addClass("bounce2");
+  
+		  function p() {
+			i.removeClass("bounce2").addClass("bounce3");
+  
+			function q() {
+			  i.removeClass("bounce3");
+			}
+			setTimeout(q, 300);
+		  }
+		  setTimeout(p, 300);
+		}
+		setTimeout(o, 300);
+	  }
+	  setTimeout(n, 300);
+	}
+  });
+  jQuery("a.page-scroll").bind("click", function (event) {
+	var $anchor = jQuery(this);
+	jQuery("html, body")
+	  .stop()
+	  .animate(
+		{
+		  scrollTop: jQuery($anchor.attr("href")).offset().top - 50
+		},
+		1250,
+		"easeInOutExpo"
+	  );
+	event.preventDefault();
+  });
+  
